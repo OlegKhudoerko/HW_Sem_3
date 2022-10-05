e8e2010789d955c34ce3604b79f134eaf3f65ebe
OlegKhudoerko <292oleg292@mail.ru>
2022-10-05 20:22:55 +0300

    add second option

--- task_3_1.py
+++ task_3_1.py
@@ -11,22 +11,36 @@
 # out
 # [4, 2, 4, 9]
 # 8
+#---------------------Вариант 1----------------------------------------------------
+from random import sample
 
-import random
 
 def sum__odd(count):
 
     if count < 0:
         return "Не корректные данные!"
 
-    ls = random.sample(range(count), count)
+    ls = sample(range(count), count)
     sum = 0
 
-    for i in range(0,count, 2):
+    for i in range(0, count, 2):
         sum += ls[i]
-            
+
     print(ls)
     return sum
 
 num = int(input("Введите число: "))
 print(sum__odd(num))
+
+#---------------------Вариант 2----------------------------------------------------
+from random_list import random_list as ls_r
+
+
+def sum_odd(count):
+    sum = 0
+    for i in range(0, len(count), 2):
+        sum += ls[i]
+    return sum
+
+ls=ls_r(int(input("Введите число: ")))
+print (ls, sum_odd(ls))
